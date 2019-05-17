b21b256d2c63ab59db8a658c4676544968817634
rupumped <rupumped@gmail.com>
2019-05-17 13:45:11 -0400

    Improved sweep over a.

--- getPixelsThatCanReach.js
+++ getPixelsThatCanReach.js
@@ -42,6 +42,13 @@ function canPixelReach(point, goal, mask, GOOD) {
 		[x1,x2] = [x2,x1];
 		[y1,y2] = [y2,y1];
 	}
+	// The x corresponding to the higher-energy waypoint.
+	var xstar = x2;
+	if (y1>y2) {
+		xstar = x1;
+	}
+	var m = (y2-y1)/(x2-x1);						// Helper variable to make eqns easier
+	var a_min = Math.max(m/(2*xstar-x1-x2), 0.01);	// Minimum a that accomplishes trajectory.
 
 	// If point and goal are directly above/below each other
 	if (x1==x2) {
@@ -68,12 +75,12 @@ function canPixelReach(point, goal, mask, GOOD) {
 
 	// If point needs parabolic arc
 	} else {
-		var m = (y2-y1)/(x2-x1);															// Helper variable to make eqns easier
 		function b(a) {return m-(x1+x2)*a;}													// The quadratic coefficient on x^1
 		function c(a) {return y1-x1*m+x1*x2*a;}												// The quadratic coefficient on x^0
 
 		// Looking for valid a
-		for (let a=0.01; a<0.7; a+=0.03+Math.log(a+1)*0.05) {								// The quadratic coefficient on x^2. Increase starting a to outlaw paths that are too shallow.
+		var a;
+		for (a=a_min; a<7*a_min; a+=a_min/2+Math.log(a+1)*a_min/2) {								// The quadratic coefficient on x^2. Increase starting a to outlaw paths that are too shallow.
 			function f(x) {return Math.round(a*x*x+b(a)*x+c(a));}							// y=ax^2+bx+x
 
 			// Exclusion Criteria: projectile path intersects with wall
@@ -90,15 +97,78 @@ function canPixelReach(point, goal, mask, GOOD) {
 				for (let y=y1; y<=y2; y++) {												// For each y from the last x to this one
 					if (Math.abs(x-point.x)>1.5*pogo.r && Math.abs(y-point.y)>1.5*pogo.r
 					 && Math.abs(x-goal.x)>1.5*pogo.r && Math.abs(y-goal.y)>1.5*pogo.r) {	// If I'm far from my start/end pt
-						if (!hasClearance(x,y,mask,pogo.r,GOOD)) {
+						if (!hasClearance(x,y,mask,2*pogo.r,GOOD)) {
 							wontIntersectWall = false;
 							break;
 						}
 					} else {
 						if (mask[x][y]!==GOOD) {											// If the point (x,y) is not traversible,
-							if (!(mask[x][y]==BORDER && (manhattan(x,y,x1,y1)<7
-							 || manhattan(x,y,x2,y2)<7))) {									// (the point is allowed to be a border if its one of the waypoints)
+							if (!(mask[x][y]==BORDER && (manhattan(x,y,point.x,point.y)<7
+							 || manhattan(x,y,goal.x,goal.y)<7))) {										// (the point is allowed to be a border if its one of the waypoints)
 								wontIntersectWall = false;									// then the trajectory does intersect with a wall
+								break;
+							}
+						}
+					}
+				}
+				if (!wontIntersectWall) {break;}
+				lastY = y;
+			}
+
+			// Exclusion Criteria: path requires too much energy
+			var withinEnergyLimit = point.y-(c(a)-b(a)*b(a)/4/a)<=MAX_DY;
+			if (!withinEnergyLimit) {return false;}
+
+			// Must both not intersect with a border and remain within the provided energy limit in order to be a valid path
+			if (wontIntersectWall && withinEnergyLimit) {
+				return f;
+			}
+		}
+
+		/*/ NICK HACK XXX
+		console.log(`${point.x},${point.y}`)
+		console.log(`${goal.x},${goal.y}`)
+		console.log(`a_min = ${a_min}`)
+		console.log(`a = ${a}`)
+		console.log('~')
+
+		var X = mask.length;
+		var Y = mask[0].length;
+		var imgData = ctx.getImageData(0,0,X,Y);
+
+		var a = a_min;
+		function f(x) {return Math.round(a*x*x+b(a)*x+c(a));}
+		for (let x=x1; x<=x2; x++) {
+			imgData = colorPixel(imgData, color[TEST], x, f(x), X, Y);
+		}
+
+		// START
+			var wontIntersectWall = true;
+			var lastY = f(x1);																// The y value of the last x 
+			for (let x=x1; x<=x2; x++) {													// For all x from start point to endpoint
+				var y = f(x);																// Compute corresponding y along ballistic trajectory
+
+				let y1 = lastY;																// Rename and ensure y2>y1
+				let y2 = y;																	// This is to make sure that, even if the slope of the trajectory is large here,
+				if (y1>y2) {																// we will still check entire path for non-traversible regions.
+					[y1,y2] = [y2,y1];
+				}
+				for (let y=y1; y<=y2; y++) {												// For each y from the last x to this one
+					if (Math.abs(x-point.x)>1.5*pogo.r && Math.abs(y-point.y)>1.5*pogo.r
+					 && Math.abs(x-goal.x)>1.5*pogo.r && Math.abs(y-goal.y)>1.5*pogo.r) {	// If I'm far from my start/end pt
+						if (!hasClearance(x,y,mask,2*pogo.r,GOOD)) {
+							wontIntersectWall = false;
+							console.log('alert 1')
+							console.log(`${x},${y}`)
+							break;
+						}
+					} else {
+						if (mask[x][y]!==GOOD) {											// If the point (x,y) is not traversible,
+							if (!(mask[x][y]==BORDER && (manhattan(x,y,point.x,point.y)<7
+							 || manhattan(x,y,goal.x,goal.y)<7))) {									// (the point is allowed to be a border if its one of the waypoints)
+								wontIntersectWall = false;		
+								console.log('alert 2')
+								console.log(`${x},${y}`)							// then the trajectory does intersect with a wall
 								break;
 							}
 						}
@@ -112,25 +182,12 @@ function canPixelReach(point, goal, mask, GOOD) {
 			var withinEnergyLimit = point.y-(c(a)-b(a)*b(a)/4/a)<=MAX_DY;
 
 			// Must both not intersect with a border and remain within the provided energy limit in order to be a valid path
+			console.log(wontIntersectWall)
+			console.log(withinEnergyLimit)
 			if (wontIntersectWall && withinEnergyLimit) {
 				return f;
 			}
-		}
-
-		/*/ NICK HACK XXX
-		console.log(`${point.x},${point.y}`)
-		console.log(`${goal.x},${goal.y}`)
-		console.log('~')
-
-		var X = mask.length;
-		var Y = mask[0].length;
-		var imgData = ctx.getImageData(0,0,X,Y);
-
-		var a = 0.04;
-		function f(x) {return Math.round(a*x*x+b(a)*x+c(a));}
-		for (let x=x1; x<=x2; x++) {
-			imgData = colorPixel(imgData, color[TEST], x, f(x), X, Y);
-		}
+		// END
 
 		ctx.putImageData(imgData, 0, 0);
 		imgData_debug = imgData;
